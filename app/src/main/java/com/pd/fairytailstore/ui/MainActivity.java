364ad1d6af89ff17494004da1cad4ffc70b7eec9
Pushin <geiby@mail.ru>
2022-11-01 23:45:17 +0300

    navigation works

--- app/src/main/java/com/pd/fairytailstore/ui/MainActivity.java
+++ app/src/main/java/com/pd/fairytailstore/ui/MainActivity.java
@@ -4,17 +4,31 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.pd.fairytailstore.Navigation;
 import com.pd.fairytailstore.R;
 
 
 public class MainActivity extends AppCompatActivity {
+    private Navigation navigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        navigation = new Navigation(getSupportFragmentManager());
         if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, StoryListFragment.newInstance()).commit();
+            navigation.addFragment(StoryListFragment.newInstance(), false);
+            //getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, StoryListFragment.newInstance()).commit();
         }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    public Navigation getNavigation() {
+        return navigation;
     }
 }
--- app/src/main/java/com/pd/fairytailstore/ui/StoryListFragment.java
+++ app/src/main/java/com/pd/fairytailstore/ui/StoryListFragment.java
@@ -2,11 +2,15 @@ package com.pd.fairytailstore.ui;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -30,6 +34,7 @@ public class StoryListFragment extends Fragment implements MyClickListener {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        setHasOptionsMenu(true); //говорим что в этом фрагменте есть своя менюшка
         View view = inflater.inflate(R.layout.fragment_storylist, container, false);
         recyclerView = view.findViewById(R.id.recycler_view_lines);        //находим ресйклер
         initRecyclerView(data, recyclerView);
@@ -56,12 +61,35 @@ public class StoryListFragment extends Fragment implements MyClickListener {
         bundle.putParcelable("ARG_STORY", faireTail);
         // Toast.makeText(getContext(), "work" + position, Toast.LENGTH_SHORT).show();
         StoryFragment fragment = StoryFragment.newInstance(faireTail); // Фрагмент, которым собираетесь заменить первый фрагмент
-        FragmentTransaction transaction = getFragmentManager().beginTransaction(); // Или getSupportFragmentManager(), если используете support.v4
+        FragmentTransaction transaction = getParentFragmentManager().beginTransaction(); // Или getSupportFragmentManager(), если используете support.v4
         transaction.replace(R.id.fragment_container, fragment); // Заменяете вторым фрагментом. Т.е. вместо метода `add()`, используете метод `replace()`
         transaction.addToBackStack(null); // Добавляете в backstack, чтобы можно было вернутся обратно
 
         transaction.commit(); // Коммитете
     }
 
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.fragment_menu, menu);
+        super.onCreateOptionsMenu(menu, inflater);
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.search_text:
+                Toast.makeText(getContext(), "работает поиск", Toast.LENGTH_SHORT).show();
+//                recyclerView.smoothScrollToPosition(data.size() - 6);
+//recyclerView.getId().
+//                adapter.notifyItemChanged(data.size() - 6);
+                return true;
+            case R.id.clear:
+                Toast.makeText(getContext(), "очищаем", Toast.LENGTH_SHORT).show();
+                data.clearAll();
+                adapter.notifyDataSetChanged();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
