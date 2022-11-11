5af0ae36963e628517bebba0fa903706bbd1a6db
Pushin <geiby@mail.ru>
2022-11-11 23:02:11 +0300

    Добавил allert dialog с отзывом о приложении

--- app/src/main/java/com/pd/fairytailstore/ui/StoryListFragment.java
+++ app/src/main/java/com/pd/fairytailstore/ui/StoryListFragment.java
@@ -1,7 +1,9 @@
 package com.pd.fairytailstore.ui;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -11,6 +13,7 @@ import android.view.ViewGroup;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -29,6 +32,7 @@ public class StoryListFragment extends Fragment implements MyClickListener {
     private StoryListAdapter adapter;
     private RecyclerView recyclerView;
     private Navigation navigation;
+    private boolean[] chosenMulti = {false, false, false, false, false, false, false};
 
     @Override
     public void onAttach(@NonNull Context context) {
@@ -55,13 +59,13 @@ public class StoryListFragment extends Fragment implements MyClickListener {
         setHasOptionsMenu(true); //говорим что в этом фрагменте есть своя менюшка
         View view = inflater.inflate(R.layout.fragment_storylist, container, false);
         recyclerView = view.findViewById(R.id.recycler_view_lines);        //находим ресйклер
-       // data = new StorySourseLocalImpl().init();// Получим источник данных для списка
+        // data = new StorySourseLocalImpl().init();// Получим источник данных для списка
 
 
         initRecyclerView(data, recyclerView);
 
-
-        if (false) {
+//Выбор базы, из кода или из Firebase
+        if (true) {
             data = new StorySourseLocalImpl().init(new StorySourselResponce() {
                 @Override
                 public void initialized(StorySourse storySourse) {
@@ -98,16 +102,7 @@ public class StoryListFragment extends Fragment implements MyClickListener {
 
         Bundle bundle = new Bundle();
         bundle.putParcelable("ARG_STORY", faireTail);
-        /*
-        // Toast.makeText(getContext(), "work" + position, Toast.LENGTH_SHORT).show();
-        StoryFragment fragment = StoryFragment.newInstance(faireTail); // Фрагмент, которым собираетесь заменить первый фрагмент
-        FragmentTransaction transaction = getParentFragmentManager().beginTransaction(); // Или getSupportFragmentManager(), если используете support.v4
-        transaction.replace(R.id.fragment_container, fragment); // Заменяете вторым фрагментом. Т.е. вместо метода `add()`, используете метод `replace()`
-        transaction.addToBackStack(null); // Добавляете в backstack, чтобы можно было вернутся обратно
 
-        transaction.commit(); // Коммитете
-
-         */
         navigation.addFragment(StoryFragment.newInstance(faireTail), true);
 
     }
@@ -123,17 +118,55 @@ public class StoryListFragment extends Fragment implements MyClickListener {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.search_text:
-                Toast.makeText(getContext(), "работает поиск", Toast.LENGTH_SHORT).show();
-//                recyclerView.smoothScrollToPosition(data.size() - 6);
-//recyclerView.getId().
-//                adapter.notifyItemChanged(data.size() - 6);
+                Toast.makeText(getContext(), "Скоро здесь будет поиск по названию сказок", Toast.LENGTH_SHORT).show();
+                /*
+                // мягко скролим на нужную позицию списка
+                recyclerView.smoothScrollToPosition(data.size() - 6);
+                adapter.notifyItemChanged(data.size() - 6);
+                 */
+
                 return true;
-            case R.id.clear:
-                Toast.makeText(getContext(), "очищаем", Toast.LENGTH_SHORT).show();
-                data.clearAll();
-                adapter.notifyDataSetChanged();
+            case R.id.review:
+                //Почему читают сказки
+                String[] variants = getResources().getStringArray(R.array.variants);
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setTitle("Почему Вы пользуетесь приложением СКАЗКИ?")
+                        .setMultiChoiceItems(variants, chosenMulti, new DialogInterface.OnMultiChoiceClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i, boolean b) {
+                                chosenMulti[i] = b;
+                                Log.d("mylogs", "Choose " + variants[i]);
+                            }
+                        })
+                        .setPositiveButton("Ответить", thanks);
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+            case R.id.about_us:
+                Toast.makeText(getContext(), "О нас", Toast.LENGTH_SHORT).show();
                 return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
+
+    private DialogInterface.OnClickListener thanks = new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialogInterface, int i) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+            builder.setMessage("Спасибо за Ваш отзыв")
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            Log.d("mylogs", "Пользователь оставил отзыв" + i);
+                        }
+                    });
+            AlertDialog dialog = builder.create();
+            dialog.show();
+
+        }
+    };
 }
+
+
